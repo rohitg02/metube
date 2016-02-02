acc8a72f2662639130328809ccc65b51ef1ff7fc
Aaron Joseph Bader <abader@titan1.cs.clemson.edu>
2016-02-02 15:28:47 -0500

    somefiles

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$dbhost = "mysql1.cs.clemson.edu";
+$dbuser = "abader4620";
+$dbpass = "projFor4620";
+$database = "Project4620";
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<body>
+		<center><?php
+			echo "<img src=https://i.imgur.com/vKXoJ4L.png/>";
+		?></center>
+		<center><form action="login.php" method="post">
+			<input type="submit" class="button"  VALUE = "Log in" >
+		</form></center>
+
+		<center><form action="register.php" method="post">
+			<input type="submit" class="button"  VALUE = "Register" >
+		</form></center>
+	</body>
+</html>
+
+
